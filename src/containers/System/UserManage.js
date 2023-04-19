8ea4b8f93c8dfc345f0857e2aeffc9c778ba3a82
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-04-20 01:08:50 +0700

    render-data-users-into-FE

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,23 +1,74 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import { getAllUsers } from '../../services/userService'
+import './UserManage.scss'
 class UserManage extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
+    }
     state = {
 
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
     }
 
-
+    /** 
+     *  Life cycle 
+     * Run component: 
+     * 1. Run constructor -> init state  
+     * 2. Did mount (set state)
+     * 3. Render 
+     */
     render() {
+        console.log('Check render ', this.state)
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center'>
+                    Manage users
+                </div>
+                <div className='users-table mt-4 mx-3'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {
+                            arrUsers && arrUsers.map((item, index) => {
+                                console.log('check map', item, index)
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className="fas fa-edit"></i></button>
+                                            <button className='btn-delete'><i className="fas fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+            </div >
         );
     }
-
 }
 
 const mapStateToProps = state => {
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,30 @@
 //The function uses the axios library to make an HTTP POST request
 import axios from '../axios'
 
+/*
+    Function: handleLoginApi
+    Description: Calls an API to handle login with the provided userEmail and userPassword
+    Parameters:
+        - userEmail: The email of the user
+        - userPassword: The password of the user
+    Return:
+        - Promise object representing the result of the HTTP request
+*/
 const handleLoginApi = (userEmail, userPassword) => {
+    // Uses axios to make a post request to the login endpoint with the email and password as data
     return axios.post('/api/login', { email: userEmail, password: userPassword });
+};
+/*
+    Function: getAllUsers
+    Description: Calls an API to get all users with the provided inputId
+    Parameter:
+        - inputId: The id of the user
+    Return:
+        - Promise object representing the result of the HTTP request
+*/
+const getAllUsers = (inputId) => {
+    //template string
+    // Uses a template string to construct the URL with the inputId
+    return axios.get(`/api/get-all-users?id=${inputId}`)
 }
-export { handleLoginApi }
+export { handleLoginApi, getAllUsers }

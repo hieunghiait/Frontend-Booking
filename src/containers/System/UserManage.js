5030851c249367e9894164cf327a738212c15bd2
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-04-23 15:20:44 +0700

    update-modal-user

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,11 +3,14 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { getAllUsers } from '../../services/userService'
 import './UserManage.scss'
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
     state = {
@@ -22,21 +25,44 @@ class UserManage extends Component {
             })
         }
     }
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
 
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
     /** 
      *  Life cycle 
      * Run component: 
      * 1. Run constructor -> init state  
-     * 2. Did mount (set state)
-     * 3. Render 
+     * 2. Did mount (set state): born; unmount
+     * 3. Render (re-render)
      */
     render() {
         console.log('Check render ', this.state)
         let arrUsers = this.state.arrUsers;
+        console.log(arrUsers)
+        // properties; nested 
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test={'abc'}
+                />
                 <div className='title text-center'>
                     Manage users
+                </div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3' onClick={() => this.handleAddNewUser()} >
+                        <i className="fas fa-user-plus"></i>
+                        Add new users
+                    </button>
                 </div>
                 <div className='users-table mt-4 mx-3'>
                     <table id="customers">

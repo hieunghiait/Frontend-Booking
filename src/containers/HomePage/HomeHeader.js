7b3bcff03eed4d4b9fc3b4e5957b3f75aa0934cb
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-05-04 12:41:46 +0700

    update translate language

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,10 +3,15 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.svg'
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils'
+import { changeLanguageApp } from '../../store/actions'
 class HomeHeader extends Component {
-
+    changeLanguage = (language) => {
+        this.props.changLanguageAppRedux(language)
+    }
     render() {
-        console.log('check props: ', this.props)
+        let language = this.props.language;
+        console.log('Check language: ', language)
         return (
             <React.Fragment>
                 <div className='home-header-container'>
@@ -40,8 +45,8 @@ class HomeHeader extends Component {
                             <div className="support"><i className="far fa-question-circle"></i>
                                 <FormattedMessage id='homeheader.support' />
                             </div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div>
@@ -50,7 +55,7 @@ class HomeHeader extends Component {
                         <div className='title1'><FormattedMessage id='banner.title1' /></div>
                         <div className='title2'><FormattedMessage id='banner.title2' /></div>
                         <div className='search'>
-                            <i class="fas fa-search"></i>
+                            <i className="fas fa-search"></i>
                             <input type='text' placeholder='Tìm lý do khám' />
                         </div>
                     </div>
@@ -109,6 +114,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
